0bd7bea4b09de3cba832da156dd5a0618b645db0
Peita <n6378056@gmail.com>
2022-02-19 01:27:57 +1000

    update readme and example

--- __init__.py
+++ __init__.py
@@ -26,12 +26,21 @@ middleman = UtilizationMiddleman(
 # 4. Create harvesters one by one, and watch utilization ratios change
 harvester_factory.create_harvester(id=0, aum_staked=5_000_000)
 harvester_factory.create_harvester(id=1, aum_staked=6_000_000)
-harvester_factory.create_harvester(id=2, aum_staked=7_000_000)
-harvester_factory.create_harvester(id=3, aum_staked=8_000_000)
-
 # activate harvester, otherwise it has no impact on emission shares
 harvester_factory.harvesters[0].activate()
 harvester_factory.harvesters[1].activate()
+
+print(middleman)
+print(harvester_factory)
+
+print("\n\t====================================================")
+print("\t************ Creating 2 more Harvesters ************")
+print("\t====================================================\n")
+
+
+harvester_factory.create_harvester(id=2, aum_staked=7_000_000)
+harvester_factory.create_harvester(id=3, aum_staked=8_000_000)
+#
 harvester_factory.harvesters[2].activate()
 harvester_factory.harvesters[3].activate()
 
