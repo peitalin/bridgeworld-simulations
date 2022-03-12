9a49c7d712022055d045c4c1d3c3eb4d4d9cb271
Peita <n6378056@gmail.com>
2022-03-12 11:47:37 +1000

    refactor treasure drop prob

--- legions/questing_crafting.py
+++ legions/questing_crafting.py
@@ -12,48 +12,38 @@ def maybe_drop_loot_from_quest(lvl):
     return drop_loot_tiers(lvl=lvl)
 
 
+# pr: probabilities of dropping t1, t2, t3, t4, t5 loot in percentages
+LOOT_DROP_PR = {
+    "easy": {
+        "t1": 0,
+        "t2": 0.025,
+        "t3": 0.05,
+        "t4": 0.15,
+        "t5": 0.775
+    },
+    "medium": {
+        "t1": 0.02,
+        "t2": 0.055,
+        "t3": 0.08,
+        "t4": 0.17,
+        "t5": 0.675
+    },
+    "hard": {
+        "t1": 0.04,
+        "t2": 0.10,
+        "t3": 0.11,
+        "t4": 0.22,
+        "t5": 0.53
+    },
+}
 
 def drop_loot_tiers(lvl='easy'):
-    # pr: probabilities of dropping t1, t2, t3, t4, t5 loot
-    # in percentages
-    if lvl=='easy':
-        pr = [0, 2.5, 5, 15, 77.5]
-    if lvl=='medium':
-        pr = [2, 5.5, 8, 17, 67.5]
-    if lvl=='hard':
-        pr = [4, 10, 11, 22, 53]
-
-    score2 = np.random.uniform(0,100)
-    # cutoffs for probability intervals
-    t1 = pr[0]
-    t2 = pr[1] + t1
-    t3 = pr[2] + t2
-    t4 = pr[3] + t3
-    t5 = pr[4] + t4
-
-    # see where RNG sample lies in between intervals
-    if score2 <= t1:
-        return 't1'
-    if t1 < score2 <= t2:
-        return 't2'
-    if t2 < score2 <= t3:
-        return 't3'
-    if t3 < score2 <= t4:
-        return 't4'
-    if t4 < score2:
-        return 't5'
+    loot_pr = LOOT_DROP_PR[lvl]
+    treasure = np.random.choice(a=list(loot_pr.keys()), p=list(loot_pr.values()))
+    return treasure
 
 
 
-# TREASURE_BREAK_PR = {
-#     "grin": 0.00597,
-#     "honeycomb": 0.00597,
-#     "t1": 0.03275,
-#     "t2": 0.06440,
-#     "t3": 0.08203,
-#     "t4": 0.11293,
-#     "t5": 0.18104,
-# }
 TREASURE_BREAK_PR = {
     "grin": 0.0,
     "honeycomb": 0.0,
@@ -63,12 +53,6 @@ TREASURE_BREAK_PR = {
     "t4": 0.113,
     "t5": 0.18104,
 }
-# t1: 3.2% => 2.7%
-# t2: 6.4% => 5.4%
-# t3: 8.2% => 6.9%
-# t4: unchanged
-# t5: unchanged
-
 
 CRAFTING_RECIPES = {
     # Prism
@@ -82,20 +66,16 @@ CRAFTING_RECIPES = {
     "medium": [
         't5', 't5', 't5', 't5', 't5',
         't4', 't4',
-        # 't3', 't3', 't3',
         't3', 't3',
-        # 't2', 't2',
         't2',
         't1',
     ],
 
     # Extractor
     "hard": [
-        # 't5', 't5', 't5', 't5', 't5',
         't5', 't5', 't5', 't5',
         't4', 't4', 't4',
         't3',
-        # 't2', 't2',
         't2',
     ],
 }
